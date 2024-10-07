fcf7325f4f574f084bf3c2687516a6ecf6b7a7d6
MingyangLiii <anfense@gmail.com>
2024-10-07 17:05:45 -0400

    fixed import path bug

--- backend/service/retriever.py
+++ backend/service/retriever.py
@@ -1,3 +1,9 @@
 class Retriever:
     def __init__(self):
+        self.index
+
+    def load_course(self, course_id):
+        index = ""
+
+    def add_section(self, course_id, section):
         pass
